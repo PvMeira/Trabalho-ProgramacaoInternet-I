742e186193986159259e2cf7fb6f198d28c883cb
PvMeira <pv.ciria@gmail.com>
2016-06-16 22:37:41 -0300

    Criando aluguel do livro

---
+++ cadastro/aluguel.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="../css/bootstrap.css">
+  <meta charset="UTF-8">
+  <title>Index</title>
+  <meta name="viewport" content="width=device-width">
+  
+</head>
+<body>
+<div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted">Portal biblioteca</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Principal</a></li>
+            <li><a href="cadastroLivro.php">Cadastro de Livro</a></li>
+            <li><a href="cadastroCliente.php">Cadastro de Clientes</a></li>
+            <li><a href="cadastroFuncionario.php">Cadastro de Funcionarios</a></li>
+            <li><a href="#">Realizar Aluguel</a></li>           
+          </ul>
+        </nav>
+      </div>
+
+ 
+<form action="../trafegoDados/processaAluguel.php" method="get">
+<fieldset>
+	<div class="form-group">
+		Livro:  <select name="book">
+		<?php
+		require("../conecta.inc");
+		conecta_bd() or die ("Não é possível conectar-se ao servidor.");
+		$resultado=mysql_query("Select * from book order by id") or die ("Não é possível consultar Funcionario.");
+		while ($linha=mysql_fetch_array($resultado))
+		{
+		$id=$linha["id"];
+		$name=$linha["name"];
+		print("<option value='$id'>$name</option>");
+		}
+		?>
+		</select>
+	</div>
+</fieldset>
+
+<fieldset>
+	<div class="form-group">
+	 Cliente:  <select name="client">
+	<?php
+			$resultado=mysql_query("Select * from client order by id") or die ("Não é possível consultar Clientes.");
+				while ($linha=mysql_fetch_array($resultado))
+				{
+					$id=$linha["id"];
+					$name=$linha["name"];
+					print("<option value='$id'>$name</option>");
+				}
+	
+	?>
+		</select>
+	</div>
+</fieldset>
+
+<fieldset>
+	<div class="form-group">
+	 Funcionario:  <select name="employee">
+	<?php
+			$resultado=mysql_query("Select * from employee order by id") or die ("Não é possível consultar Funcionario.");
+				while ($linha=mysql_fetch_array($resultado))
+				{
+					$id=$linha["id"];
+					$name=$linha["name"];
+					print("<option value='$id'>$name</option>");
+				}
+	
+	?>
+		</select>
+	</div>
+</fieldset>
+<fieldset>
+	<div class="form-group">
+	Data: <input type="text" name="date" placeholder="dd-mm-aaaa">
+	</div>
+</fieldset>
+	<button type="submit" class="btn btn-primary">
+		<span class="glyphicon glyphicon-floppy-saved"></span>
+		Confirmar Cadastro
+	</button>
+</form>
+</div>
+  
+</div>
+  
+</body>
+</html>
---
+++ trafegoDados/processaAluguel.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="../css/bootstrap.css">
+  <meta charset="UTF-8">
+  <title>Aluguel</title>
+  <meta name="viewport" content="width=device-width">
+  
+</head>
+<body>
+<div class="container">
+
+      <div class="masthead">
+        <h3 class="text-muted">Portal biblioteca</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="../cadastro/principal.php">Principal</a></li>
+            <li><a href="../cadastro/cadastroLivro.php">Cadastro de Livro</a></li>
+            <li><a href="../cadastro/cadastroCliente.php">Cadastro de Clientes</a></li>
+            <li><a href="../cadastro/cadastroFuncionario.php">Cadastro de Funcionarios</a></li>
+            <li><a href="#">Realizar Aluguel</a></li>           
+          </ul>
+        </nav>
+      </div>
+<?php
+$client=$_GET['client'];
+$employee=$_GET['employee'];
+$book=$_GET['book'];
+$status='Alugado';
+$date =$_GET['date'];
+
+if ($client=='' or $employee=='' or $book=='' or $date=='')
+	print("Faltou preencher algum campo!");
+else
+{
+	require("../conecta.inc");
+	conecta_bd() or die ("Não é possível conectar-se ao servidor.");
+	print("Realizando o aluguel:<p>");
+	mysql_query("insert into rent (id_client, id_book, id_employee, avaliable, rentDate) values ('$client', '$book', '$employee', '$status','$date')") 
+	or die ("Não é possível realizar aluguel!");
+		mysql_query("update  book set stash='-1' where id='$book'") 
+		or die ("Não é possível alterar o estoque !");
+	print("Aluguel realizado com sucesso:");
+}
+?>
+ 
+</body>
+</html>
