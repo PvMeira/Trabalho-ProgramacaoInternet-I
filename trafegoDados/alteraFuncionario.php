77bec584f54fec8cadaf91c23f5bc67be22995ce
PvMeira <pv.ciria@gmail.com>
2016-06-18 09:39:31 -0300

    Melhorando pagina de alteração (Livro, Funcionario e cliente)

--- trafegoDados/alteraFuncionario.php
+++ trafegoDados/alteraFuncionario.php
@@ -1,12 +1,31 @@
+<!doctype html>
 <html>
 <head>
 <link rel="stylesheet" href="../css/bootstrap.css">
   <meta charset="UTF-8">
-  <title>Deletar Cliente</title>
+  <title>Index</title>
   <meta name="viewport" content="width=device-width">
   
 </head>
 <body>
+<div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted">Portal biblioteca</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Principal</a></li>
+            <li><a href="cadastroLivro.php">Cadastro de Livro</a></li>
+            <li><a href="cadastroCliente.php">Cadastro de Clientes</a></li>
+            <li><a href="cadastroFuncionario.php">Cadastro de Funcionarios</a></li>
+            <li><a href="#">Realizar Aluguel</a></li>           
+          </ul>
+        </nav>
+      </div>
+
+      <div>
 <?php
 	$cod=$_GET['cod'];
 	require("../conecta.inc");
@@ -18,21 +37,31 @@
 	$cpf=$linha ["cpf"];
 	$mail=$linha ["mail"];
 	$address=$linha ["address"];
-	print("<h3>Alterando os dados do Funcioanrio:</h3><p>");
 ?>
 <form action="confirmaAlteraracaoFuncionario.php" method="get">
-Código: <?php print($id)?>
-<input type="hidden" name="id_alter" value="<?php print($id)?>">
-<br>Nome: <input type="text" name="name_alter" value="<?php print($name)?>">
-
-<br>Nome: <input type="text" name="cpf_alter" value="<?php print($cpf)?>">
-
-<br>Nome: <input type="text" name="mail_alter" value="<?php print($mail)?>">
-
-<br>Nome: <input type="text" name="address_alter" value="<?php print($address)?>">
+	<table class='display table' width='90%'>
+			<tr><td><b>Código</td>
+			<td><b>Nome</td>
+			<td><b>cpf</td>
+			<td><b>mail</td>
+			<td><b>Endereço</td>
+			<tr><td><input type="hidden" name="id_alter" value="<?php print($id)?>"></td>
+			<td><input type="text" name="name_alter" value="<?php print($name)?>"></td>
+			<td><input type="text" name="cpf_alter" value="<?php print($cpf)?>"></td>
+			<td><input type="text" name="mail_alter" value="<?php print($mail)?>"></td>
+			<td><input type="text" name="address_alter" value="<?php print($address)?>"></td>
+			</table></center>
 
 <p><input type="submit" value="Alterar Dados">
 </form>
 <p><a href="../visualizacao/visualizacaoFuncionario.php">Cancelar e voltar</a>
+ </div>
+      
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; 2016 Company, Inc.Todos os direitos reservados ao time de desenvolvimento</p>
+      </footer>
+
+    </div> <!-- /container -->
 </body>
 </html>
--- trafegoDados/alteraLivro.php
+++ trafegoDados/alteraLivro.php
@@ -1,12 +1,31 @@
+<!doctype html>
 <html>
 <head>
 <link rel="stylesheet" href="../css/bootstrap.css">
   <meta charset="UTF-8">
-  <title>Alterar Livro</title>
+  <title>Index</title>
   <meta name="viewport" content="width=device-width">
   
 </head>
 <body>
+<div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted">Portal biblioteca</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#">Principal</a></li>
+            <li><a href="cadastroLivro.php">Cadastro de Livro</a></li>
+            <li><a href="cadastroCliente.php">Cadastro de Clientes</a></li>
+            <li><a href="cadastroFuncionario.php">Cadastro de Funcionarios</a></li>
+            <li><a href="#">Realizar Aluguel</a></li>           
+          </ul>
+        </nav>
+      </div>
+
+      <div>
 <?php
 	$cod=$_GET['cod'];
 	require("../conecta.inc");
@@ -18,21 +37,32 @@
 	$writer=$linha ["writer"];
 	$pages=$linha ["pages"];
 	$stash=$linha ["stash"];
-	print("<h3>Alterando os dados do Livro:</h3><p>");
 ?>
 <form action="confirmaAlteraracaoLivro.php" method="get">
-Código: <?php print($id)?>
-<input type="hidden" name="id_alter" value="<?php print($id)?>">
-<br>Nome: <input type="text" name="name_alter" value="<?php print($name)?>">
+	<table class='display table' width='90%'>
+			<tr><td><b>Código</td>
+			<td><b>Autor</td>
+			<td><b>Paginas</td>
+			<td><b>Estoque</td>
+			<td><b>Nome</td>
+			<tr><td><input type="hidden" name="id_alter" value="<?php print($id)?>"></td>
+			<td><input type="text" name="writer_alter" value="<?php print($writer)?>"></td>
+			<td><input type="text" name="pages_alter" value="<?php print($pages)?>"></td>
+			<td><input type="text" name="stash_alter" value="<?php print($stash)?>"></td>
+			<td><input type="text" name="name_alter" value="<?php print($name)?>"></td>
+	</table></center>
 
-<br>Escritor: <input type="text" name="writer_alter" value="<?php print($writer)?>">
-
-<br>Paginas: <input type="text" name="pages_alter" value="<?php print($pages)?>">
-
-<br>Estoque: <input type="text" name="stash_alter" value="<?php print($stash)?>">
-
-<p><input type="submit" value="Alterar Dados">
+			<p><input type="submit" value="Alterar Dados">
 </form>
-<p><a href="mostrad.php">Cancelar e voltar</a>
+<p><a href="../visualizacao/visualizacaoLivro.php">Cancelar e voltar</a>
+
+</div>
+      
+      <!-- Site footer -->
+      <footer class="footer">
+        <p>&copy; 2016 Company, Inc.Todos os direitos reservados ao time de desenvolvimento</p>
+      </footer>
+
+    </div> <!-- /container -->
 </body>
 </html>
