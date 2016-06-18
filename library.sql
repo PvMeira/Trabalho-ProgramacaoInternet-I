54762b38946e76fc9a7a3d759df9fa814f911bdb
PvMeira <pv.ciria@gmail.com>
2016-06-18 12:25:51 -0300

    Adicionando dump da bibliteca

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 18/06/2016 às 14h16min
+-- Tempo de Geração: 18/06/2016 às 17h25min
 -- Versão do Servidor: 5.5.20
 -- Versão do PHP: 5.3.9
 
@@ -40,10 +40,9 @@ CREATE TABLE IF NOT EXISTS `book` (
 --
 
 INSERT INTO `book` (`id`, `name`, `writer`, `pages`, `stash`) VALUES
-(3, 'livro_1', 'teste Autor', '199', 0),
-(4, 'livro_2', 'teste Autor', '199', 13),
-(5, 'livro_3', 'teste Autor', '199', 15),
-(6, 'livro_4', 'teste Autor', '199', 14),
+(3, 'livro_1', 'teste Autor ', '199', 0),
+(4, 'livro_2', 'teste Autor', '199', 12),
+(6, 'livro_4', 'teste Autor', '199', 13),
 (7, 'livro_5', 'teste Autor', '199', 15);
 
 -- --------------------------------------------------------
@@ -61,14 +60,15 @@ CREATE TABLE IF NOT EXISTS `client` (
   `rentCount` int(11) NOT NULL,
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   UNIQUE KEY `is` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `client`
 --
 
 INSERT INTO `client` (`name`, `phone`, `mail`, `cpf`, `address`, `rentCount`, `id`) VALUES
-('JoÃ£o Paulo de Zouza', '(51)321456789', 'Zouza@teste.com', '86874787878', 'Rua Palmeiras do pinhal', 3, 3);
+('JoÃ£o Paulo de Zouza ', '(51)321456789', 'Zouza@teste.com', '86874787878', 'Rua Palmeiras do pinhal', 4, 3),
+('Pedro Vitor ', '(51)321317896', 'pc.stefa@not.com', '38388383838838', 'Rua Palmeiras do pinhal', 1, 4);
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 
 INSERT INTO `employee` (`name`, `phone`, `mail`, `cpf`, `address`, `id`) VALUES
-('teste', '5454545455', 'pv.ciria@gmail.com', '86874787878', '', 2);
+('teste', '5454545455', 'pv.ciria@gmail.com', '86874787878', 'HEYY', 2);
 
 -- --------------------------------------------------------
 
@@ -107,7 +107,7 @@ CREATE TABLE IF NOT EXISTS `rent` (
   `id_employee` int(11) DEFAULT NULL,
   `avaliable` varchar(50) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Extraindo dados da tabela `rent`
@@ -117,7 +117,10 @@ INSERT INTO `rent` (`id`, `rentDate`, `id_client`, `id_book`, `id_employee`, `av
 (20, '11/11/2222', 3, 5, 2, 'Devolvido'),
 (21, '11/11/2222', 3, 6, 2, 'Devolvido'),
 (22, '11/11/2222', 3, 6, 2, 'Devolvido'),
-(23, '11/11/2222', 3, 6, 2, 'Devolvido');
+(23, '11/11/2222', 3, 6, 2, 'Devolvido'),
+(24, '11/11/2222', 3, 4, 2, 'Devolvido'),
+(25, '11/11/2017', 3, 6, 2, 'Alugado'),
+(26, '11/11/2015', 4, 4, 2, 'Alugado');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
